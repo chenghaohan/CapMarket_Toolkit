515d895def1c0658c6dcfe2a5d2a6c865bc96b8d
chenghaohan <71295207+chenghaohan@users.noreply.github.com>
2021-03-23 21:10:25 -0400

    company info update

--- website template/js/demo/company.js
+++ website template/js/demo/company.js
@@ -1,9 +1,4 @@
-const getFormattedCurrency = (currency, amount) => new Intl.NumberFormat(undefined, {
-    style: 'currency',
-    currency,
-  }).format(amount);
-  
-  // Submit Button handler
+    // Submit Button handler
   function handleSubmit() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
@@ -15,7 +10,6 @@ const getFormattedCurrency = (currency, amount) => new Intl.NumberFormat(undefin
   d3.select("#stockInput").node().value = "";
   
   updateCompany(stock);
-  createMap(stock);
 
   }
   
@@ -23,70 +17,102 @@ const getFormattedCurrency = (currency, amount) => new Intl.NumberFormat(undefin
   
   function init () {
   updateCompany("AAPL");
-  createMap("AAPL");
 
   }
   
   //-----------------Function to build the balance sheet--------------------------------
   function updateCompany (stock){
     
-    var url = url = `https://financialmodelingprep.com/api/v3/profile/${stock}?apikey=e22042cb161e2d1917a06964e0036d91`;
+    var url = `https://financialmodelingprep.com/api/v3/profile/${stock}?apikey=e22042cb161e2d1917a06964e0036d91`;
 
     d3.json(url).then(function (data) {
+
+        //update company name and introduction and link and image 
 
         var company = data[0].companyName;
         var intro = data[0].description;
         var link = data[0].website;
+        var image = data[0].image;
 
         document.getElementById("co_name").innerHTML =company;
-        
         document.getElementById("intro").innerHTML =intro;
-
         document.getElementById("co_name").href = link;
-        
-
-        console.log(link);
+        document.getElementById("myImg").src = image;
 
 
+
+        // update company summary table
+        var ceo = data[0].ceo;
+        var industry = data[0].industry;
+        var isin = data[0].isin;
+        var cusip = data[0].cusip;
+        var exh_mkt = data[0].exchangeShortName;
+        var p_range = data[0].range;
+        var beta = data[0].beta;
+        var dividend = data[0].lastDiv;
+        var ipo = data[0].ipoDate;
+
+        var myTable = document.getElementById('myTable');
+
+        myTable.rows[0].cells[1].innerHTML = ceo;
+        myTable.rows[1].cells[1].innerHTML = industry;
+        myTable.rows[2].cells[1].innerHTML = isin;
+        myTable.rows[3].cells[1].innerHTML = cusip;
+        myTable.rows[4].cells[1].innerHTML = exh_mkt;
+        myTable.rows[5].cells[1].innerHTML = p_range;
+        myTable.rows[6].cells[1].innerHTML = beta;
+        myTable.rows[7].cells[1].innerHTML = dividend;
+        myTable.rows[8].cells[1].innerHTML = ipo;
 
     })
-
-
-    
-
 };
 //-------------------Function to build the income statement-----------------------------------------------------------------
 
-function createMap (stock){
+// function updateTable (stock){
 
-    url = `https://financialmodelingprep.com/api/v3/profile/${stock}?apikey=e22042cb161e2d1917a06964e0036d91`;
+//     var url = `https://financialmodelingprep.com/api/v3/profile/${stock}?apikey=e22042cb161e2d1917a06964e0036d91`;
     
-    d3.json(url).then(function (data){
+//     d3.json(url).then(function (data){
 
-        var add = data.map(d => d.address);
-        var city = data.map(d => d.city);
-        var state = data.map(d => d.state);
-        var country = data.map(d => d.country);
-        var address = `${add}, ${city}, ${state}, ${country}`
-
-        google.maps.event.addDomListener(window, 'load', intilize);
-
-        function intilize (){
-            var autocomplete = new google.maps.places.Autocomplete(address);
-            google.maps.event.addListener(autocomplete, 'place_changed', function (){
-                var place = autocomplete.getPlace();
-                var lat = place.geometry.location.A;
-                var lng = place.geometry.location.F;
-                var coord = [lat, lng];
-                console.log(coord)
-            });
-    };
-});
+//         var ceo = json
 
 
-    
 
-        
+
+
+//     });
+
+
+   
+// };
+
+//--------------------------------------------------------------------------------------------------------------------------
+  // Add event listener for submit button
+  d3.select("#submit").on("click", handleSubmit);
+  // initiate the function
+  init();
+  
+  //---------------------------------------------------------------------------------------------------------------------
+  
+//         var add = data.map(d => d.address);
+//         var city = data.map(d => d.city);
+//         var state = data.map(d => d.state);
+//         var country = data.map(d => d.country);
+//         var address = `${add}, ${city}, ${state}, ${country}`
+
+//         google.maps.event.addDomListener(window, 'load', intilize);
+
+//         function intilize (){
+//             var autocomplete = new google.maps.places.Autocomplete(address);
+//             google.maps.event.addListener(autocomplete, 'place_changed', function (){
+//                 var place = autocomplete.getPlace();
+//                 var lat = place.geometry.location.A;
+//                 var lng = place.geometry.location.F;
+//                 var coord = [lat, lng];
+//                 console.log(coord)
+//             });
+//     };
+// });   
     // }
 
 
@@ -107,15 +133,3 @@ function createMap (stock){
     //     id: "mapbox/streets-v11",
     //     accessToken: MAP_KEY
     // }).addTo(myMap);
-   
-};
-
-//--------------------------------------------------------------------------------------------------------------------------
-  // Add event listener for submit button
-  d3.select("#submit").on("click", handleSubmit);
-  // initiate the function
-  init();
-  
-  //---------------------------------------------------------------------------------------------------------------------
-  
-  
